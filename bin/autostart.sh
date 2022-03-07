d567b29e7380bbfd1a1e0dcd2e88656dbaea927f
edjubert <edouard.jubert@gmail.com>
2022-03-07 17:31:56 +0100

    blured light version

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -5,7 +5,7 @@ killall -9 polybar conky dunst
 dunst --config /home/edjubert/dunst/dunstrc &
 
 # Polybar
-$HOME/.config/i3/bin/launchbar.sh
+# $HOME/.config/i3/bin/launchbar.sh
 
 # Conky
 conky -c ~/.config/i3/conky/cpu_panel
